44f98f6b32dfc649a0495992ed2eea171783232b
Ahsan Ali <ahsanalijamali369@gmail.com>
2026-01-18 10:28:10 +0500

    Add files via upload
    A simple Python project that downloads YouTube videos using a video link. Just run the program, paste the link, and the video will be downloaded using the yt_dlp library.

---
+++ video.py
@@ -0,0 +1,14 @@
+import yt_dlp
+
+url= input("Enter the video URL: ")
+
+ydl_opts = {
+
+
+    'outtmpl': '%(title)s.%(ext)s'
+  
+  }
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+
+print("Download completed.")
